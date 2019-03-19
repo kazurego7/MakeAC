b9062bf02c31b1a61d8d8be1b28d9dea23ec5ad7
kazurego7 <kazurego7@gmail.com>
2019-03-19 18:15:47 +0900

    mkabcのテンプレートをconsoleからabctempに

--- mkabc.bat
+++ mkabc.bat
@@ -1,7 +1,7 @@
 @echo off 
 mkdir ABC%1
 cd ABC%1
-dotnet new console -o A
-dotnet new console -o B
-dotnet new console -o C
-dotnet new console -o D
+dotnet new abctemp -o A
+dotnet new abctemp -o B
+dotnet new abctemp -o C
+dotnet new abctemp -o D
