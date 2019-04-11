9dea778420698cd5bd2d1ea564c2099895578822
kazurego7 <kazurego7@gmail.com>
2019-04-11 15:34:37 +0900

    mkabc.batで、同名フォルダが存在しているときは、中止するようにした

--- mkabc.bat
+++ mkabc.bat
@@ -1,4 +1,7 @@
 @echo off
+if exist ABC%1 (
+    echo "ABC%1 has exist. use other name."
+) else (
 mkdir ABC%1
 cd ABC%1
 dotnet new actemp -o A
@@ -6,3 +9,4 @@ dotnet new actemp -o B
 dotnet new actemp -o C
 dotnet new actemp -o D
 cd ../
+)
