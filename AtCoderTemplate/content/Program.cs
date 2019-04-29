efc960bbd916a5aca89ef00b6a32bb4b53ff22cb
kazurego7 <kazurego7@gmail.com>
2019-04-29 23:44:14 +0900

    BigInteger入れようかと思ったけどやっぱやめる

--- AtCoderTemplate/content/Program.cs
+++ AtCoderTemplate/content/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using static System.Math;
 using static AtCoderTemplate.MyExtensions;
 using static AtCoderTemplate.MyInputOutputs;
@@ -19,10 +18,6 @@ namespace AtCoderTemplate {
         }
         public static long ReadLong () {
             return long.Parse (Console.ReadLine ());
-        }
-
-        public static BigInteger ReadInteger () {
-            return BigInteger.Parse (Console.ReadLine ());
         }
         public static List<int> ReadInts () {
             return Console.ReadLine ().Split (' ').Select (c => int.Parse (c)).ToList ();
