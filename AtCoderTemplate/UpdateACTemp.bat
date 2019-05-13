390c768c98551ec216dad583d654f0c0c95757b1
kazurego7 <kazurego7@gmail.com>
2019-05-14 00:03:36 +0900

    gitignore反映のためキャッシュを削除

--- AtCoderTemplate/UpdateACTemp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-pushd "%~dp0"
-if exist *.nupkg (
-    del *.nupkg
-    )
-nuget pack ./AtCoderContest.Template.CSharp.nuspec -Build -NoDefaultExcludes
-dotnet new -i ./*.nupkg
-popd
