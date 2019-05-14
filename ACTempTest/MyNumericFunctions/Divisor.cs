4d7082d6927c150402b3a31bf9d54b7ccd5a2d12
kazurego7 <kazurego7@gmail.com>
2019-05-14 13:35:48 +0900

    やっぱりTheoryDataの方がよさそうやね。......

--- ACTempTest/MyNumericFunctions/Divisor.cs
+++ ACTempTest/MyNumericFunctions/Divisor.cs
@@ -9,15 +9,17 @@ using static AtCoderTemplate.MyNumericFunctions;
 namespace MyNumericFunctions.Test {
     public class Divisor {
         // 値のテスト用のデータ作成メソッド
-        public static IEnumerable<object[]> GetValueData () {
-            var ToObj = ToObjectCaster<List<long>, long>.MakeCast ();
-            return new List<object[]> {
-                ToObj (new List<long> { 1, 2, 3, 6 }, 6), // 正の整数
-                ToObj (new List<long> { 1, 13 }, 13), // 素数
-                ToObj (new List<long> { 1, 2, 4 }, 4), // 2乗根が整数になる（ため、2乗根までしか計算してない、とかで）
-                ToObj (new List<long> { 1 }, 1) // 下限
+        public static IEnumerable<object[]> GetValueData =>
+            new TheoryData<IEnumerable<long>, long> {
+                // 正の整数
+                { new List<long> { 1, 2, 3, 6 }, 6 },
+                // 素数
+                { new List<long> { 1, 13 }, 13 },
+                // 2乗根が整数になる（ため、2乗根までしか計算してない、とかで）
+                { new List<long> { 1, 2, 4 }, 4 },
+                // 下限
+                { new List<long> { 1 }, 1 },
             };
-        }
 
         // 値のテスト
         [Theory]
@@ -27,13 +29,13 @@ namespace MyNumericFunctions.Test {
         }
 
         // 例外のテスト用のデータ作成メソッド
-        public static IEnumerable<object[]> GetExceptionData () {
-            var ToObj = ToObjectCaster<long>.MakeCast ();
-            return new List<object[]> {
-                ToObj (0), // 0
-                ToObj (-6) // 負の数
+        public static IEnumerable<object[]> GetExceptionData =>
+            new TheoryData<long> {
+                // 0
+                { 0 },
+                // 負の数
+                {-6 },
             };
-        }
 
         [Theory]
         [MemberData (nameof (GetExceptionData))]
