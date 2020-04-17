103c2fab6d7be6bc3d04755b2bec3f319add7ab8
Kazuki Wada <kazurego7@gmail.com>
2020-04-18 00:50:21 +0900

    feat/ テンプレートを複数作れるようにした

--- MakeAC/Program.cs
+++ MakeAC/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Text.Json;
+
+using ACIgnore = System.Collections.Generic.Dictionary<System.String, System.String>;
 
 // Entrypoint, create from the .NET Core Console App.
 class Program : ConsoleAppBase // inherit ConsoleAppBase
@@ -26,14 +29,16 @@ class Program : ConsoleAppBase // inherit ConsoleAppBase
     }
 
     [Command("install", "テンプレートのインストール")]
-    public void InstallTemplate([Option(0, "テンプレートとなるプロジェクトへのパス")]string templatePath)
+    public void InstallTemplate([Option(0, "テンプレート名")]string templateName, [Option(1, "テンプレートとなるプロジェクトへのパス")]string templatePath)
     {
         var invalidPathString = new string(Path.GetInvalidPathChars());
         if (invalidPathString.Any(invalidChar => templatePath.Contains(invalidChar)))
         {
-            Console.Error.WriteLine("WA! 以下の文字は、パスに含むことができません");
+            Console.Error.WriteLine("WA! 以下の文字は、テンプレートとなるプロジェクトへのパスに含むことができません。");
             Console.Error.WriteLine($"{String.Join(" ", invalidPathString)}");
+            return;
         }
+
         if (!Directory.Exists(templatePath))
         {
             Console.Error.WriteLine($"WA! {templatePath} は存在しません。");
@@ -41,36 +46,81 @@ class Program : ConsoleAppBase // inherit ConsoleAppBase
             return;
         }
 
-        if (Path.IsPathRooted(templatePath))
+        var templateAbsolutePath = Path.GetFullPath(templatePath);
+
+        if (!File.Exists(configFilePath))
         {
-            File.WriteAllText(configFilePath, templatePath);
+            var templates = new ACIgnore();
+            templates.Add(templateName, templateAbsolutePath);
+            File.WriteAllText(configFilePath, JsonSerializer.Serialize<ACIgnore>(templates));
+            Console.WriteLine($"AC! テンプレート名 {templateName} に、{templateAbsolutePath} をインストールしました。");
         }
         else
         {
-            File.WriteAllText(configFilePath, Path.GetFullPath(templatePath));
+            var templates = JsonSerializer.Deserialize<ACIgnore>(File.ReadAllText(configFilePath));
+            if (!templates.ContainsKey(templateName))
+            {
+                templates.Add(templateName, templateAbsolutePath);
+                File.WriteAllText(configFilePath, JsonSerializer.Serialize<ACIgnore>(templates));
+                Console.WriteLine($"AC! テンプレート名 {templateName} に、{templateAbsolutePath} をインストールしました。");
+            }
+            else
+            {
+                Console.WriteLine($"テンプレート名 {templateName} には、{templates[templateName]} が既にインストールされています。");
+                Console.WriteLine("パスを上書きしますか？ (yes/no)");
+                var input = Console.ReadLine();
+                if (input == "yes")
+                {
+                    templates[templateName] = templateAbsolutePath;
+                    File.WriteAllText(configFilePath, JsonSerializer.Serialize<ACIgnore>(templates));
+                    Console.WriteLine($"AC! テンプレート名 {templateName} に、{templateAbsolutePath} を上書きインストールしました。");
+                }
+                else
+                {
+                    Console.WriteLine($"テンプレート名 {templateName} を上書きせず終了しました。");
+                }
+            }
         }
-
-        Console.WriteLine($"AC! {templatePath} をテンプレートとして登録しました。");
     }
 
     [Command("list", "インストールしたテンプレートの一覧")]
     public void ListTemplate()
     {
-        if (!File.Exists(configFilePath) || File.ReadAllText(configFilePath) == "")
+        if (!File.Exists(configFilePath))
         {
-            Console.Error.WriteLine($"WA! install コマンドで、利用するテンプレートプロジェクトのパスを指定してください。");
+            Console.Error.WriteLine($"WA! install コマンドで、テンプレートをインストールしてください。");
             return;
         }
 
-        var templatePath = File.ReadAllText(configFilePath);
-
-        Console.WriteLine(templatePath);
+        Console.WriteLine($"テンプレート名 : テンプレートへのパス");
+        foreach (var template in JsonSerializer.Deserialize<ACIgnore>(File.ReadAllText(configFilePath)))
+        {
+            Console.WriteLine($"{template.Key} : {template.Value}");
+        }
     }
 
-    [Command("create", "コンテスト用のプロジェクト作成")]
-    public void CreateContestProjects([Option(0, "コンテスト名")]string contestName)
+    [Command("new", "コンテスト用のプロジェクト作成")]
+    public void CreateContestProjects([Option(0, "利用するテンプレート名")]string templateName, [Option(1, "作成するコンテスト名")]string contestName)
     {
-        Console.WriteLine("WJ... コンテスト名のディレクトリを作成します。");
+        if (!File.Exists(configFilePath))
+        {
+            Console.Error.WriteLine($"WA! install コマンドで、テンプレートをインストールしてください。");
+            return;
+        }
+
+        var templates = JsonSerializer.Deserialize<ACIgnore>(File.ReadAllText(configFilePath));
+        if (!templates.ContainsKey(templateName))
+        {
+            Console.Error.WriteLine($"WA! {templateName} がテンプレートに存在しません。");
+            Console.Error.WriteLine("テンプレート一覧を確認してください。");
+
+            Console.WriteLine($"テンプレート名 : テンプレートへのパス");
+            foreach (var template in templates)
+            {
+                Console.WriteLine($"{template.Key} : {template.Value}");
+            }
+            return;
+        }
 
         var invalidFileNameString = new string(Path.GetInvalidFileNameChars());
         if (invalidFileNameString.Any(invalidChar => contestName.Contains(invalidChar)))
@@ -85,13 +135,7 @@ class Program : ConsoleAppBase // inherit ConsoleAppBase
             return;
         }
 
-        if (!File.Exists(configFilePath) || File.ReadAllText(configFilePath) == "")
-        {
-            Console.Error.WriteLine($"WA! install コマンドで、利用するテンプレートプロジェクトのパスを指定してください。");
-            return;
-        }
-
-        var templatePath = File.ReadAllText(configFilePath);
+        Console.WriteLine("WJ... コンテスト名のディレクトリを作成します。");
 
         Directory.CreateDirectory(contestName);
         foreach (var problemName in new List<string> { "A", "B", "C", "D", "E", "F" })
@@ -99,7 +143,7 @@ class Program : ConsoleAppBase // inherit ConsoleAppBase
             Console.WriteLine($"WJ... {problemName} ディレクトリを作成します。");
 
             var problemPath = Path.Combine(contestName, problemName);
-            Task.Run(() => ProcessX.StartAsync($"cp -r {templatePath}  {problemPath}").WriteLineAllAsync());
+            Task.Run(() => ProcessX.StartAsync($"cp -r {templates[templateName]}  {problemPath}").WriteLineAllAsync());
         }
 
         Console.WriteLine("AC! コンテスト用の各問題プロジェクトの作成が完了しました。");
